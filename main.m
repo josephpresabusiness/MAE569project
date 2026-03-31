fd0e4d54b8dd18a6b4fbf10e2805f91a56e495df
josephpresabusiness <joseph.presa.business@gmail.com>
2026-03-31 13:58:27 -0500

    main func param tweaks

--- main.m
+++ main.m
@@ -16,7 +16,7 @@ ub = [747652,... % launch time
 
 % differential evolution options
 opts.pop_size = 200;
-opts.max_gen  = 1000;
+opts.max_gen  = 100;
 opts.F        = 0.8;
 opts.CR       = 0.9;
 
@@ -24,11 +24,13 @@ fitness_fn = @(x) fitness(x);
 [x_best, dv_best] = differential_evolution(fitness_fn, lb, ub, opts);
 
 fprintf('Best solution: %.3f km/s total ΔV\n', dv_best);
+fprintf('best state:');
+disp(x_best);
 
 % Polish with gradient descent
-options = optimset('MaxFunEvals', 50000, 'TolFun', 1e-6);
-[x_polished, dv_polished] = fminsearch(fitness_fn, x_best, options);
-
-fprintf('true best: %.3f km/s total ΔV\n', dv_polished);
-fprintf('best state:');
-disp(x_polished);
+% options = optimset('MaxFunEvals', 50000, 'TolFun', 1e-6);
+% [x_polished, dv_polished] = fminsearch(fitness_fn, x_best, options);
+% 
+% fprintf('true best: %.3f km/s total ΔV\n', dv_polished);
+% fprintf('best state:');
+% disp(x_polished);
