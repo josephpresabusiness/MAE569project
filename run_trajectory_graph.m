a9a42ad9cf903e9b111a03829d06a1034cd33ba4
josephpresabusiness <joseph.presa.business@gmail.com>
2026-04-08 11:57:48 -0500

    convenient script to see final trajectory

---
+++ run_trajectory_graph.m
@@ -0,0 +1,5 @@
+x_perfect = [741827.1571,... % launch time
+        3.1826, 3.6709, 11.9643, 258.0782,... % transfer time, TU
+        100000, 100000, 299935.857,... % flyby height, km
+        0,1,1,0]; % path type
+trajectory_graph(x_perfect);
